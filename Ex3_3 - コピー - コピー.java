0889cd7423a3765ed371b2d9b2a1d6fad0cb5a3f
atsushi otsuka <bigmounda@gmail.com>
2025-09-22 13:15:15 +0900

    Ex3_3ファイル追加

---
+++ Ex3_3 - コピー - コピー.java
@@ -0,0 +1,19 @@
+public class Ex3_3{
+    public static void main(String[] args) {
+        int isHungry = 0;
+        String food = "おにぎり";
+        
+        System.out.println("①こんにちは");        
+        if(isHungry == 0){
+        System.out.println("②お腹がいっぱいです");
+        } else{
+        System.out.println("③はらぺこです");
+        }
+        if(isHungry == 1) {
+            System.out.println(food + "をいただきます");
+        }
+        System.out.println("④ごちそうさまでした");
+    }
+}
+
+// kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk コメント
